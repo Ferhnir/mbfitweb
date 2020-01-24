943db2499146f5c4f08b982fdfab97780c9ed9ea
Ferhnir <zax1984@gmail.com>
2020-01-24 06:47:39 +0000

    Added Testimoniales page

--- src/pages/subpages/whoHasMadeIt.vue
+++ src/pages/subpages/whoHasMadeIt.vue
@@ -1,17 +1,26 @@
 <template>
   <q-layout class="flex flex-center column text-y">
     <div class="bg-black column q-py-xl q-page-gaps">
+      <div class="row q-pa-xl">
+        <div class="col col-xs-12">
+          <p class="text-h3 text-center text-weight-bold text-y">
+            LOOK WHO HAS MADE IT
+          </p>
+        </div>
+      </div>
       <q-carousel
         v-model="slide"
         transition-prev="scale"
         transition-next="scale"
+        prev-icon="arrow_left"
+        next-icon="arrow_right"
         swipeable
         animated
         control-color="yellow"
         navigation
         padding
         arrows
-        height="400px"
+        height="480px"
         infinite
         class="bg-black text-white shadow-1 rounded-borders"
       >
@@ -21,7 +30,11 @@
           v-for="(card, index) in testimonialsObj"
           :key="index"
         >
-          <q-card class="my-card bg-black text-y q-pt-xl" flat bordered>
+          <q-card
+            class="my-card bg-black text-y q-pt-xl flex-center"
+            flat
+            bordered
+          >
             <q-item>
               <q-item-section avatar>
                 <q-avatar>
@@ -31,6 +44,7 @@
 
               <q-item-section>
                 <q-item-label>{{ card.user }}</q-item-label>
+                <q-item-label caption>{{ card.spec }}</q-item-label>
               </q-item-section>
             </q-item>
             <q-card-section>
@@ -45,13 +59,6 @@
             <q-separator />
           </q-card>
         </q-carousel-slide>
-        <div class="row q-pa-md">
-          <div class="col col-xs-12">
-            <p class="text-h3 text-center text-weight-bold text-y">
-              LOOK WHO HAS MADE IT
-            </p>
-          </div>
-        </div>
       </q-carousel>
     </div>
   </q-layout>
@@ -65,13 +72,15 @@ export default {
       testimonialsObj: [
         {
           user: "Psikuta bez S",
+          spec: "Technican",
           userID: 1,
           desc:
-            "When i started i could not do a single push up, now i am pushing equivalent of my age 40 x. Coach Ben was clear in the when i will achieve that..Super happy.",
+            "When i started i could not do a single push up, now i am pushing equivalent of my age 40 x. Coach Ben was clear in the when i will achieve that..Super happy. When i started i could not do a single push up, now i am pushing equivalent of my age 40 x. Coach Ben was clear in the when i will achieve that..Super happy.",
           rate: 5
         },
         {
           user: "Psikuta 123bez S",
+          spec: "Teacher",
           userID: 2,
           desc:
             "Learned so much and still learning as my physique is keeping progressing..Coach Ben is fun to with.",
@@ -79,33 +88,10 @@ export default {
         },
         {
           user: "Psikuta 45645645bez S",
+          spec: "Salesman",
           userID: 3,
           desc:
             "Been with Coach Ben for 15 Weeks and cannot recommend him enough..Got my body back and we are not finished yet.",
-          rate: 5
-        },
-        {
-          user: "Psikuta 45645645bez S",
-          userID: 4,
-          desc: "asdasdasdasdas das das das das das das das dasdasd asd asd a",
-          rate: 5
-        },
-        {
-          user: "Psikuta 45645645bez S",
-          userID: 5,
-          desc: "asdasdasdasdas das das das das das das das dasdasd asd asd a",
-          rate: 5
-        },
-        {
-          user: "Psikuta 45645645bez S",
-          userID: 6,
-          desc: "asdasdasdasdas das das das das das das das dasdasd asd asd a",
-          rate: 5
-        },
-        {
-          user: "Psikuta 45645645bez S",
-          userID: 7,
-          desc: "asdasdasdasdas das das das das das das das dasdasd asd asd a",
           rate: 5
         }
       ]
